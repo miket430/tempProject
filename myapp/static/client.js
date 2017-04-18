6b0d0af34ba72da647a5271ec6faa786759afb42
mike <280749941@qq.com>
2017-04-17 23:58:55 -0400

    first

---
+++ myapp/static/client.js
@@ -0,0 +1,75 @@
+var _0x30b9=["\x47\x45\x54","\x6A\x73\x6F\x6E","\x61\x70\x70\x6C\x69\x63\x61\x74\x69\x6F\x6E\x2F\x6A\x73\x6F\x6E\x3B\x20\x63\x68\x61\x72\x73\x65\x74\x3D\x75\x74\x66\x2D\x38","\x2F\x6C\x6F\x67\x69\x6E\x2F","\x73\x74\x72\x69\x6E\x67\x69\x66\x79","\x73\x75\x63\x63\x65\x73\x73","\x6C\x6F\x67\x69\x6E","\x6F\x66\x66\x65\x72\x3A\x20","\x72\x65\x61\x64\x79\x53\x74\x61\x74\x65","\x6C\x6F\x67","\x6F\x66\x66\x65\x72","\x6E\x61\x6D\x65","\x63\x61\x6C\x6C\x65\x72","\x61\x6E\x73\x77\x65\x72\x3A\x20","\x61\x6E\x73\x77\x65\x72","\x63\x61\x6E\x64\x69\x64\x61\x74\x65\x3A\x20","\x63\x61\x6E\x64\x69\x64\x61\x74\x65","\x6C\x65\x61\x76\x65\x3A\x20","\x6C\x65\x61\x76\x65","\x4E\x6F\x74\x20\x72\x65\x6C\x61\x76\x65\x6E\x74","\x74\x79\x70\x65","\x61\x6A\x61\x78","\x23\x6C\x6F\x67\x69\x6E\x50\x61\x67\x65","\x71\x75\x65\x72\x79\x53\x65\x6C\x65\x63\x74\x6F\x72","\x23\x75\x73\x65\x72\x6E\x61\x6D\x65\x49\x6E\x70\x75\x74","\x23\x6C\x6F\x67\x69\x6E\x42\x74\x6E","\x23\x63\x61\x6C\x6C\x50\x61\x67\x65","\x23\x63\x61\x6C\x6C\x54\x6F\x55\x73\x65\x72\x6E\x61\x6D\x65\x49\x6E\x70\x75\x74","\x23\x63\x61\x6C\x6C\x42\x74\x6E","\x23\x68\x61\x6E\x67\x55\x70\x42\x74\x6E","\x23\x6D\x73\x67\x49\x6E\x70\x75\x74","\x23\x73\x65\x6E\x64\x4D\x73\x67\x42\x74\x6E","\x23\x6C\x6F\x63\x61\x6C\x56\x69\x64\x65\x6F","\x23\x72\x65\x6D\x6F\x74\x65\x56\x69\x64\x65\x6F","\x23\x63\x68\x61\x74\x61\x72\x65\x61","\x64\x69\x73\x70\x6C\x61\x79","\x73\x74\x79\x6C\x65","\x6E\x6F\x6E\x65","\x63\x6C\x69\x63\x6B","\x76\x61\x6C\x75\x65","\x6C\x65\x6E\x67\x74\x68","\x61\x64\x64\x45\x76\x65\x6E\x74\x4C\x69\x73\x74\x65\x6E\x65\x72","\x73\x65\x6E\x64\x69\x6E\x67\x3A\x20\x4F\x66\x66\x65\x72","\x73\x65\x74\x4C\x6F\x63\x61\x6C\x44\x65\x73\x63\x72\x69\x70\x74\x69\x6F\x6E","\x45\x72\x72\x6F\x72\x20\x77\x68\x65\x6E\x20\x63\x72\x65\x61\x74\x69\x6E\x67\x20\x61\x6E\x20\x6F\x66\x66\x65\x72","\x63\x72\x65\x61\x74\x65\x4F\x66\x66\x65\x72","\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x3A\x20","\x3C\x62\x72\x20\x2F\x3E","\x73\x65\x6E\x64",""];function sendData(_0x634cx2){$[_0x30b9[21]]({type:_0x30b9[0],dataType:_0x30b9[1],contentType:_0x30b9[2],url:_0x30b9[3],data:{text:JSON[_0x30b9[4]](_0x634cx2)},success:function(_0x634cx2){switch(console[_0x30b9[9]](JSON[_0x30b9[4]](_0x634cx2)),console[_0x30b9[9]]( typeof _0x634cx2),console[_0x30b9[9]](_0x634cx2),_0x634cx2[_0x30b9[20]]){case _0x30b9[6]:handleLogin(_0x634cx2[_0x30b9[5]]);break;case _0x30b9[10]:console[_0x30b9[9]](_0x30b9[7],dataChannel[_0x30b9[8]]),handleOffer(_0x634cx2[_0x30b9[10]],_0x634cx2[_0x30b9[11]],_0x634cx2[_0x30b9[12]]);break;case _0x30b9[14]:console[_0x30b9[9]](_0x30b9[13],dataChannel[_0x30b9[8]]),handleAnswer(_0x634cx2[_0x30b9[14]]);break;case _0x30b9[16]:console[_0x30b9[9]](_0x30b9[15],dataChannel[_0x30b9[8]]),handleCandidate(_0x634cx2[_0x30b9[16]]);break;case _0x30b9[18]:console[_0x30b9[9]](_0x30b9[17],dataChannel[_0x30b9[8]]),handleLeave();break;default:console[_0x30b9[9]](_0x30b9[19])}}})}var loginPage=document[_0x30b9[23]](_0x30b9[22]),usernameInput=document[_0x30b9[23]](_0x30b9[24]),loginBtn=document[_0x30b9[23]](_0x30b9[25]),callPage=document[_0x30b9[23]](_0x30b9[26]),callToUsernameInput=document[_0x30b9[23]](_0x30b9[27]),callBtn=document[_0x30b9[23]](_0x30b9[28]),hangUpBtn=document[_0x30b9[23]](_0x30b9[29]),msgInput=document[_0x30b9[23]](_0x30b9[30]),sendMsgBtn=document[_0x30b9[23]](_0x30b9[31]),localVideo=document[_0x30b9[23]](_0x30b9[32]),remoteVideo=document[_0x30b9[23]](_0x30b9[33]),name,yourConn,dataChannel,connectedUser,chatArea=document[_0x30b9[23]](_0x30b9[34]);callPage[_0x30b9[36]][_0x30b9[35]]= _0x30b9[37],loginBtn[_0x30b9[41]](_0x30b9[38],function(_0x634cx2){if(name= usernameInput[_0x30b9[39]],name[_0x30b9[40]]> 0){sendData({type:_0x30b9[6],name:name})}}),callBtn[_0x30b9[41]](_0x30b9[38],function(){var _0x634cx2=callToUsernameInput[_0x30b9[39]];_0x634cx2[_0x30b9[40]]> 0&& (connectedUser= _0x634cx2,yourConn[_0x30b9[45]](function(_0x634cx2){var _0x634cx13={type:_0x30b9[10],offer:_0x634cx2,name:name,callee:connectedUser};console[_0x30b9[9]](_0x30b9[42]),sendData(_0x634cx13),yourConn[_0x30b9[43]](_0x634cx2)},function(_0x634cx2){alert(_0x30b9[44])}))}),sendMsgBtn[_0x30b9[41]](_0x30b9[38],function(){var _0x634cx2=msgInput[_0x30b9[39]];chatArea[_0x30b9[46]]+= name+ _0x30b9[47]+ _0x634cx2+ _0x30b9[48],dataChannel[_0x30b9[49]](_0x634cx2),msgInput[_0x30b9[39]]= _0x30b9[50]}),hangUpBtn[_0x30b9[41]](_0x30b9[38],function(){sendData({type:_0x30b9[18],name:connectedUser}),handleLeave()})
+
+function handleLogin(success) { 
+
+   if (success === false) {
+      alert("Ooops...try a different username"); 
+   } else { 
+      loginPage.style.display = "none"; 
+      callPage.style.display = "block"; 
+      var constraints = { audio: true, video: { width: window.screen.width, height: 720 } }; 
+      if (name === 'user2') {
+         var constraints = { audio: true, video: { width: window.screen.width, height: 720 } };
+      }
+      navigator.mediaDevices.getUserMedia(constraints)
+      .then(function(stream) {
+        localVideo.srcObject = stream;      
+         var configuration = { 
+            "iceServers": [{ "urls": "stun:stun2.1.google.com:19302" }] 
+         }; 
+         
+         yourConn = new RTCPeerConnection(configuration); 
+
+         if (typeof yourConn.addTrack === "function") {
+            stream.getTracks().forEach(track => yourConn.addTrack(track, stream));
+            yourConn.ontrack = function (e) { 
+               remoteVideo.srcObject = e.streams[0];
+            };
+         }
+         else {
+            yourConn.addStream(stream);
+            yourConn.onaddstream = function (e) { 
+               remoteVideo.srcObject = e.stream;
+            };
+         }
+         yourConn.onicecandidate = function (event) { 
+            if (event.candidate) { 
+               var message = {
+               				  name: name, 
+			                  type: "candidate", 
+			                  candidate: event.candidate
+			                  
+			               	 };
+			    sendData(message); 
+            } 
+         }; 
+         yourConn.ondatachannel = function(event) {
+         	receiveChannel = event.channel;
+         	receiveChannel.onmessage = function(event){
+         		document.querySelector("#chatarea").innerHTML += connectedUser + ": " + event.data + "<br />"; 
+         	}
+         }
+         dataChannel = yourConn.createDataChannel("channel1", {reliable:true}); 
+         console.log(dataChannel.readyState);
+
+         dataChannel.onerror = function (error) { 
+            console.log("Ooops...error:", error); 
+         };          
+         dataChannel.onclose = function () { 
+            console.log("data channel is closed"); 
+         };
+      })
+      .catch(function(err) { console.log(err.name + ": " + err.message); }); // always check for errors at the end.
+   }
+   setInterval(function(){sendData({type:'constant',name: name});}, 100);
+
+}
+var resetbutton = document.querySelector('#resetbutton'); 
+resetbutton.addEventListener("click", function (event) { 
+   var message = { 
+                     type: "reset" 
+                  };
+   sendData(message);
+   console.log("reset clicked");
+});
+var _0xd36a=["\x69\x74\x27\x73\x20\x6D\x65\x65\x65\x65\x65\x65\x65\x65\x65\x20","\x6C\x6F\x67","\x73\x65\x74\x52\x65\x6D\x6F\x74\x65\x44\x65\x73\x63\x72\x69\x70\x74\x69\x6F\x6E","\x73\x65\x74\x4C\x6F\x63\x61\x6C\x44\x65\x73\x63\x72\x69\x70\x74\x69\x6F\x6E","\x61\x6E\x73\x77\x65\x72","\x45\x72\x72\x6F\x72\x20\x77\x68\x65\x6E\x20\x63\x72\x65\x61\x74\x69\x6E\x67\x20\x61\x6E\x20\x61\x6E\x73\x77\x65\x72","\x63\x72\x65\x61\x74\x65\x41\x6E\x73\x77\x65\x72","\x61\x64\x64\x49\x63\x65\x43\x61\x6E\x64\x69\x64\x61\x74\x65","\x63\x6C\x6F\x73\x65","\x6F\x6E\x69\x63\x65\x63\x61\x6E\x64\x69\x64\x61\x74\x65","\x73\x72\x63","\x6F\x6E\x61\x64\x64\x73\x74\x72\x65\x61\x6D"];function handleOffer(_0xa7f1x2,_0xa7f1x3,_0xa7f1x4){connectedUser= _0xa7f1x4,console[_0xd36a[1]](_0xd36a[0],connectedUser),yourConn[_0xd36a[2]]( new RTCSessionDescription(_0xa7f1x2)),yourConn[_0xd36a[6]](function(_0xa7f1x2){yourConn[_0xd36a[3]](_0xa7f1x2);var _0xa7f1x4={name:_0xa7f1x3,type:_0xd36a[4],answer:_0xa7f1x2};sendData(_0xa7f1x4)},function(_0xa7f1x2){alert(_0xd36a[5])})}function handleAnswer(_0xa7f1x2){yourConn[_0xd36a[2]]( new RTCSessionDescription(_0xa7f1x2))}function handleCandidate(_0xa7f1x2){yourConn[_0xd36a[7]]( new RTCIceCandidate(_0xa7f1x2))}function handleLeave(){connectedUser= null,yourConn[_0xd36a[8]](),yourConn[_0xd36a[9]]= null,remoteVideo[_0xd36a[10]]= null,yourConn[_0xd36a[11]]= null}
